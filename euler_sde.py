dc3b76ddcdc1e7d53cd751e9cc77237ebc958223
Todd Krause <toddbkrause@gmail.com>
2011-07-22 18:00:56 -0500

    Euler method for SDE; minor corrections

--- euler_sde.py
+++ euler_sde.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 
-# A short script to simulate a Wiener process
+# A short script to simulate an SDE via
+# the Euler discretization scheme
 
 import sys
 import math as m
 import numpy as np
 import matplotlib.pyplot as plt
 
+def a(y, t):
+    return t*y
+
+def b(y, t):
+    return m.sqrt(t) * m.sin(y)
+
 n_steps = 100
 delta_t = 0.01
+x_0     = 0.5
 
 while len(sys.argv) > 1:
     option = sys.argv[1]
@@ -20,17 +28,21 @@ while len(sys.argv) > 1:
     elif option == '-dt':
         delta_t = float(sys.argv[1])
         del sys.argv[1]
+    elif option == '-x':
+        x_0 == float(sys.argv[1])
+        del sys.argv[1]
     else:
         print sys.argv[0], ': Invalid option', option
         sys.exit(1)
 
 times = np.linspace(0, n_steps * delta_t, n_steps)
 
-W = [0.0]
+y = [x_0]
 for t in times[1:]:
     dt = delta_t
     Z = np.random.normal(0,1)
-    W.append(W[-1] + Z * m.sqrt(dt))
+    dW = Z * m.sqrt(dt)
+    y.append(y[-1] + a(y[-1],t) * dt + b(y[-1],t) * dW)
 
-plt.plot(times, list(W))
+plt.plot(times, y)
 plt.show()
